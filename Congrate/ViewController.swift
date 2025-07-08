e74de77307091731c07f39343bb089bb866b4356
prammmoe <ikhwanpramuditha05@gmail.com>
2025-07-08 20:25:24 +0700

    Initial Commit

---
+++ Congrate/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Congrate
+//
+//  Created by Pramuditha Muhammad Ikhwan on 08/07/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ CongrateTests/CongrateTests.swift
@@ -0,0 +1,17 @@
+//
+//  CongrateTests.swift
+//  CongrateTests
+//
+//  Created by Pramuditha Muhammad Ikhwan on 08/07/25.
+//
+
+import Testing
+@testable import Congrate
+
+struct CongrateTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
